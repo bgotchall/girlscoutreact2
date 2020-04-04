b3042ace853530978db2c0ef0dcccc7c486d7afd
austinev <bgotchall@gmail.com>
2020-04-04 02:07:01 -0500

    adding back in all the -- i think-- essential stuff

--- index.js
+++ index.js
@@ -17,13 +17,22 @@ app.get('/api/getList', (req,res) => {
   console.log('Sent list of items');
 });
 
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+require("./routes/news-routes.js")(app);
+require("./routes/test-routes.js")(app);
+require("./routes/header-routes.js")(app);
 
-console.log(`Password generator listening on ${port}`);
+var PORT = process.env.PORT || 5000;
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: false }).then(function() {
+    app.listen(5000, function() {
+      console.log("server listening on PORT " + PORT);
+    });
+  });
